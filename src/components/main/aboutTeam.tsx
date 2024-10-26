0e11b65f0eee92043ae9332c8ad2e4a008d4f7f4
Danilo Romão <htpps.sw@gmail.com>
2024-10-26 13:35:28 -0300

    fix

--- src/components/main/aboutTeam.tsx
+++ src/components/main/aboutTeam.tsx
@@ -33,11 +33,11 @@ function TeamMemberCard({ name, role, src }: TeamMemberCardProps) {
 }
 
 const teamMembers = [
-     { id: 1, name: "Ana Lyce", role: "Ceo do Sexo", src: AnaImg.src },
-     { id: 2, name: "Duda", role: "Mestre dos magos", src: DudaImg.src },
-     { id: 3, name: "Hendry", role: "Ultima romantica", src: HendryImg.src },
-     { id: 4, name: "Kevin", role: "Nerd esquisito", src: KevinImg.src },
-     { id: 5, name: "Rhaina", role: "Dona de parque", src: RaiImg.src },
+     { id: 1, name: "Ana Lyce", role: "Ceo", src: AnaImg.src },
+     { id: 2, name: "Duda", role: "Diretora de Marketing", src: DudaImg.src },
+     { id: 3, name: "Hendry", role: "Diretora de Operações", src: HendryImg.src },
+     { id: 4, name: "Kevin", role: "Diretora de Tecnologia", src: KevinImg.src },
+     { id: 5, name: "Rhaina", role: "Diretora de Relações", src: RaiImg.src },
 ];
 
 export default function AboutTeam() {
