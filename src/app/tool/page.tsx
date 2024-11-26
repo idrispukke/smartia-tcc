e3758b2c025790ac7bfb7f552ceae9831353c0c4
Danilo <htpps.sw@gamil.com>
2024-11-26 11:48:29 -0300

    change url

--- src/app/tool/page.tsx
+++ src/app/tool/page.tsx
@@ -82,7 +82,7 @@ export default function Component() {
   }) => {
     startTransition(async () => {
       try {
-        const response = await axios.post('http://localhost:3000/api/tool', {
+        const response = await axios.post('https://smartia.vercel.app/api/tool', {
           ideia: data.ideia,
           tipo: data.tipo,
           plataforma: data.plataforma,
