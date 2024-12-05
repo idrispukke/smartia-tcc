4dccbf0643bbb6717e53479a974dcf35824e3de7
Keven de Oliveira <66386728+idrispukke@users.noreply.github.com>
2024-12-05 12:09:18 -0300

    grammar fix

--- src/components/main/AboutEnterprise.tsx
+++ src/components/main/AboutEnterprise.tsx
@@ -11,21 +11,14 @@ export default function AboutEnterprise() {
                }}>
                <div className="relative z-10 flex flex-col items-center h-screen  pt-10 mdpt-16 pb-2  max-w-2xl px-4 space-y-4">
                     <h2 className="text-3xl md:text-4xl font-bold">Sobre a empresa</h2>
-                    <p className="md:text-lg  md:w-[67%] text-center">Uma empresa totalmente dedicada e apaixonada pelos nossos clientes!</p>
+                    <p className="md:text-lg  md:w-[67%] text-center">Uma empresa dedicada e apaixonada pelos nossos clientes!</p>
 
                     {/* Bloco de Descrição com Fundo Semi-transparente */}
                     <div className="bg-black bg-opacity-60 rounded-xl p-6 md:text-lg text-justify leading-relaxed shadow-lg">
                          <p>
-                              A empresa Smart.ia se dedica com um compromisso inabalável com a qualidade e a inovação. Ela não apenas atende às expectativas dos seus clientes, mas frequentemente as supera. Desde sua fundação em 2024, tem sido sinônimo de integridade, dedicação e inovação, contando com uma equipe excepcional.
+                              Nossa empresa, a Smart.ia, tem um compromisso com a qualidade e a inovação. Não apenas atendemos às expectativas dos clientes, mas frequentemente as superamos. Desde nossa fundação em 2024, temos sido sinônimo de integridade, dedicação e inovação, contando com uma equipe excepcional.
                          </p>
                     </div>
-
-                    <Link
-                         href="/ferramenta"
-                         className=" py-2 px-6 text-3xl bg-gradient-to-r from-blue-500 to-cyan-400 rounded-lg hover:from-blue-600 hover:to-cyan-500 "
-                    >
-                         Saiba Mais 
-                    </Link>
                </div>
           </section >
      )
